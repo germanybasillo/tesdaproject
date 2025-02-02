73474ed340481b8d1167122665f982e90945da57
germany <germanybasillo@gmail.com>
2025-02-03 06:32:02 +0800

    first commit

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -29,7 +29,7 @@
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-blue-400 hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
                         
                         
-                        <img src="{{ asset(Auth::user()->logo) }}" alt="User Logo" style="width: 40px; height: 40px; border-radius: 50%; object-fit: cover;">
+                        <div><img src="{{ asset(Auth::user()->logo) }}" alt="User Logo" style="width: 40px; height: 40px; border-radius: 50%; object-fit: cover;"></div>
     
                          <!-- @if (Auth::user()->role === 'admin')
                          <div class="text-white font-bold">
