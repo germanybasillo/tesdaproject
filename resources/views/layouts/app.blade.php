3d2d83076f75490b914da35fbae422f1ef775c8c
germany <germanybasillo@gmail.com>
2025-02-05 14:17:02 +0800

    first commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,10 +11,8 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
-        <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/pagestyle/css/style.css', 'resources/pagestyle/js/script.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
@@ -34,49 +32,6 @@
                 {{ $slot }}
             </main>
         </div>
-
-        <script>
-            // Navigate Back to Step 1
-function goBackToStep1() {
-    document.getElementById('step2').style.display = 'none';
-    document.getElementById('step1').style.display = 'block';
-}
-        </script>
-
-<script>
-    function toggleDocumentField() {
-        var trainingStatus = document.getElementById('training_status').value;
-        var oropfafnsContainer = document.getElementById('oropfafnsContainer');
-
-        if (trainingStatus === 'non-scholar') {
-            oropfafnsContainer.style.display = 'block';
-        } else {
-            oropfafnsContainer.style.display = 'none';
-        }
-    }
-</script>
-
-<script>
-    // Document Preview for Uploaded PDF
-function previewDocument(event, containerId, iframeId) {
-    const file = event.target.files[0];
-    const previewContainer = document.getElementById(containerId);
-    const previewIframe = document.getElementById(iframeId);
-
-    if (file) {
-        if (file.type === 'application/pdf') {
-            const fileURL = URL.createObjectURL(file);
-            previewIframe.src = fileURL;
-            previewContainer.style.display = 'block';
-        } else {
-            alert('Please upload a valid PDF document.');
-            previewContainer.style.display = 'none';
-        }
-    } else {
-        previewContainer.style.display = 'none';
-    }
-}
-</script>
 
     </body>
 </html>
